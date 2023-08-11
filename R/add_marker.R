2fd4a2dd2cb1e48b4f63bd0ec322ee6388a19615
Richard Hanna <richardshanna91@gmail.com>
2023-08-11 16:27:02 -0400

    Update add_markers :checkered_flag:

--- R/add_marker.R
+++ R/add_marker.R
@@ -4,9 +4,10 @@
 #' A short description...
 #'
 #' @param ggswim_obj a ggswim_obj
-#' @param data data where markers reside
+#' @param data data where markers reside, if in ggswim object leave `NULL` (default)
 #' @param id the id variable associated with the marker
 #' @param time the timestamp location for where to place the markers
+#' @param name the name of the marker. Default `NULL`
 #' @param ... Other arguments passed on to layer(). These are often aesthetics,
 #' used to set an aesthetic to a fixed value, like colour = "red" or size = 3. They may also be parameters to the paired geom/stat.
 #'
@@ -18,20 +19,35 @@
 
 add_marker <- function(
     ggswim_obj,
-    data,
+    data = NULL,
     id,
     time,
+    name,
     ...
     ) {
   # Capture variables as expressions, allowing for piping in API
-  variables <- c("id", "time")
+  variables <- c("id", "time", "name")
 
   # Parse variables to be passed to streamline()
   for (variable in variables) {
     assign(variable, eval(parse(text = paste0("enquo(", variable, ") |> get_expr()"))))
   }
 
+  # If no new dataset applied, use data from the original ggswim_obj
+  if (is.null(data)) {
+    data <- ggswim_obj$data
+  }
+
+  # Add a fill reference point
+  data_new <- data |>
+    mutate(
+      fill = name
+    )
+
+  # TODO: Make this uniquitous? Currently copied in ggswim_new function
+  data_new[[id]] <- factor(data_new[[id]], levels = rev(unique(data_new[[id]])))
+
   ggswim_obj +
-    geom_point(data = data, aes(x = !!time, y = !!id), ...)
+    geom_point(data = data_new, aes(x = !!time, y = !!id, fill = fill), ...)
 
 }
