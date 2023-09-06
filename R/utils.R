449a3e2fd6bcacd7b40a66b178623f671150f64d
Richard Hanna <richardshanna91@gmail.com>
2023-09-06 10:45:02 -0400

    Remove get_ggswim_obj and insert_override fxs :x:
    No longer necessary for current method of fixing legend via `fix_legend()`

--- R/utils.R
+++ R/utils.R
@@ -25,7 +25,7 @@ get_layer_data <- function(data, mapping, i = 1L) {
   # Starting with color/colour, since that will always need to be given to result in a legend layer for ggsegment
   aes_mapping <- unlist(mapping)
 
-  #TODO: Currently functionality is limited to and requires a color or fill aesthetic
+  # TODO: Currently functionality is limited to and requires a color or fill aesthetic
   if (any(c("color", "colour") %in% names(aes_mapping))) {
     colour_or_color <- ifelse("colour" %in% names(aes_mapping), "colour", "color")
     colour_mapping <- data[[aes_mapping[[colour_or_color]] |> get_expr()]]
@@ -41,87 +41,16 @@ get_layer_data <- function(data, mapping, i = 1L) {
   }
 
   if (!is.null(colour_mapping)) {
-    #TODO: Unsure we can ever guarantee this is correct... but might be a reasonable assumption
+    # TODO: Unsure we can ever guarantee this is correct... but might be a reasonable assumption
     layer_data <- cbind(layer_data(i = i), colour_mapping) |>
       arrange(colour_mapping) # Assume correct since ggplot legend is arranged this way
   }
 
   if (!is.null(fill_mapping)) {
-    #TODO: Unsure we can ever guarantee this is correct... but might be a reasonable assumption
+    # TODO: Unsure we can ever guarantee this is correct... but might be a reasonable assumption
     layer_data <- cbind(layer_data(i = i), fill_mapping) |>
       arrange(fill_mapping) # Assume correct since ggplot legend is arranged this way
   }
 
   layer_data
-}
-
-#' @title Insert override layer element
-#'
-#' @description
-#' A helper function that wraps `get_layer_data` and adds the result to a ggplot
-#' layer.
-#'
-#' @returns A ggplot layer object appended with the `overrides` list object
-#'
-#' @keywords internal
-#'
-#' @param data The data responsible for the current layer
-#' @param current_layer An integer value corresponding to the current working layer
-#' @param mapping Set of aesthetic mappings created by `aes()`. If specified and
-#' `inherit.aes = TRUE` (the default), it is combined with the default mapping
-#' at the top level of the plot. You must supply mapping if there is no plot mapping.
-#' @param ignore_mapping A vector of characters for mapping values to ignore.
-#' Will typically match "required" `aes` elements from ggplot geom functions
-
-insert_override <- function(
-    data,
-    current_layer,
-    mapping,
-    ignore_mapping
-) {
-  out <- list()
-  layer_data <- get_layer_data(data, mapping, i = current_layer)
-
-  # Subset aesthetic definitions mapped that aren't required by the geom
-  used_mapping_names <- setdiff(names(mapping), ignore_mapping)
-
-  # Assign named list elements based on the used mapping elements
-  out<- vector("list", length(used_mapping_names))
-  names(out) <- used_mapping_names
-
-  # Assign the corresponding values to those mapping elements
-  for (map_element in used_mapping_names) {
-    out[[map_element]] <- c(out[[map_element]], unique(layer_data[[map_element]])) # keep any pre-existing elements
-  }
-
-  out
-}
-
-#' @title Get ggswim object from an enviornment variable
-#'
-#' @description
-#' Determine the parent environment component containing a `ggswim_obj` class
-#' type and return the object
-#'
-#' @keywords internal
-#'
-#' @returns A ggswim object
-#'
-#' @param env an environment object
-
-get_ggswim_obj_from_env <- function(env) {
-  sub_objects <- ls(env)
-
-  ggswim_object <- list()
-
-  # Loop through sub-objects and classify them
-  for (sub_object in sub_objects) {
-    obj <- get(sub_object, envir = env)
-    obj_class <- class(obj)
-    if ("ggswim_obj" %in% obj_class) {
-      ggswim_object <- obj
-    }
-  }
-
-  ggswim_object
 }
