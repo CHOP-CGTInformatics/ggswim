58b48584b62b0e41ba8652be298b88af5c1c7eef
Richard Hanna <richardshanna91@gmail.com>
2023-09-19 19:17:40 -0400

    Fix actions issues :wrench:

--- R/ggswim-package.R
+++ R/ggswim-package.R
@@ -6,10 +6,10 @@
 #' @importFrom rlang caller_arg caller_env as_label is_atomic get_expr .data
 #'   is_empty :=
 #' @importFrom dplyr arrange bind_rows select arrange any_of
-#' @importFrom checkmate check_logical check_list check_integerish
-#' @importFrom("stats", "setNames") check_data_frame
+#' @importFrom checkmate check_logical check_list check_integerish check_data_frame check_character
+#' @importFrom stats setNames
 #' @importFrom vdiffr expect_doppelganger
-#'   check_character
+#' @importFrom tidyr pivot_longer
 "_PACKAGE"
 
 ## usethis namespace: start
