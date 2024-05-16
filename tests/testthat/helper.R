5056fe730cb4a4a9ea6a3471f76c5a1458628a32
Richard Hanna <richardshanna91@gmail.com>
2024-05-16 14:21:14 -0400

    Add new tests and snapshots :sparkles:

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -15,8 +15,10 @@ simple_plot <- function() {
     new_scale_color() +
     geom_swim_label(
       data = end_study_events,
-      mapping = aes(x = time_from_initial_infusion, y = pt_id, # nolint: object_usage_linter
-                    label_vals = end_study_label, label_names = end_study_name), # nolint: object_usage_linter
+      mapping = aes(
+        x = time_from_initial_infusion, y = pt_id, # nolint: object_usage_linter
+        label_vals = end_study_label, label_names = end_study_name # nolint object_usage_linter
+      ),
       label.size = NA, size = 5
     ) +
     ggplot2::scale_color_brewer(name = "Labels", palette = 1) +
---
+++ tests/testthat/test-geom_swim_label.R
@@ -0,0 +1,38 @@
+test_that("geom_swim_label is the right class", {
+  layer <- geom_swim_label(
+    data = end_study_events,
+    mapping = aes(
+      x = time_from_initial_infusion, y = pt_id,
+      label_vals = end_study_label,
+      label_names = end_study_name
+    )
+  )
+
+  expect_equal(attr(layer, "class"), "marker_label")
+})
+
+test_that("all expected attributes exist in geom_swim_label", {
+  layer <- geom_swim_label(
+    data = end_study_events,
+    mapping = aes(
+      x = time_from_initial_infusion, y = pt_id,
+      label_vals = end_study_label,
+      label_names = end_study_name
+    )
+  )
+
+  attrs <- names(attributes(layer))
+
+  expected_attrs <- c(
+    "class",
+    "stat",
+    "position",
+    "mapping",
+    "data",
+    "show.legend",
+    "inherit.aes",
+    "params"
+  )
+
+  expect_true(all(expected_attrs %in% attrs))
+})
