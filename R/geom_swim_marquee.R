4169ce6c72f45d7028be541b37f3e7bc39142cbe
Richard Hanna <richardshanna91@gmail.com>
2024-09-18 16:39:10 -0400

    text/marquee function comparison

--- R/geom_swim_marquee.R
+++ R/geom_swim_marquee.R
@@ -10,7 +10,7 @@
 #'
 #' @export
 
-geom_swim_marker <- function(mapping = NULL, data = NULL,
+geom_swim_marquee <- function(mapping = NULL, data = NULL,
                               stat = "identity", position = "identity",
                               ...,
                               color = NULL,
@@ -20,13 +20,13 @@ geom_swim_marker <- function(mapping = NULL, data = NULL,
                               show.legend = NA,
                               inherit.aes = TRUE) {
   # Set up marker_key object containing information for the legend setup
-  # and color overwriting in the setup_data portion of GeomSwimMarker
+  # and color overwriting in the setup_data portion of GeomSwimMarquee
   marker_labels <- rlang::eval_tidy(data = data, expr = mapping$label)
 
   marker_key <- data.frame(
     marker_labels = marker_labels,
     marker_glyphs = glyph,
-    # label_glyph = paste0("{.", color, " ", glyph, "}"),
+    label_glyph = paste0("{.", color, " ", glyph, "}"),
     marker_colors = color
   ) |> distinct()
 
@@ -34,7 +34,7 @@ geom_swim_marker <- function(mapping = NULL, data = NULL,
     data = data,
     mapping = mapping,
     stat = StatMarker,
-    geom = GeomSwimMarker,
+    geom = GeomSwimMarquee,
     position = position,
     show.legend = show.legend,
     inherit.aes = inherit.aes,
@@ -47,7 +47,7 @@ geom_swim_marker <- function(mapping = NULL, data = NULL,
   )
 
   # Reclass the layer to trigger ggplot_add and apply manual scale/guide
-  class(layer_obj) <- c("swim_marker", class(layer_obj))
+  class(layer_obj) <- c("swim_marquee", class(layer_obj))
   layer_obj$marker_key <- marker_key
   layer_obj
 }
@@ -65,7 +65,8 @@ StatMarker <- ggplot2::ggproto(
 )
 
 #' @export
-ggplot_add.swim_marker <- function(object, plot, object_name) {
+ggplot_add.swim_marquee <- function(object, plot, object_name) {
+
   marker_key <- object$marker_key |>
     arrange(marker_labels)
 
@@ -85,25 +86,26 @@ ggplot_add.swim_marker <- function(object, plot, object_name) {
     )
 }
 
-#' @rdname geom_swim_marker
+#' @rdname geom_swim_marquee
 #' @format NULL
 #' @usage NULL
 #' @export
-GeomSwimMarker <- ggproto("GeomSwimMarker", GeomText,
+GeomSwimMarquee <- ggproto("GeomSwimMarquee", GeomMarquee,
                            required_aes = c("x", "y", "label"),
 
                            draw_panel = function(self, data, panel_params, coord, size.unit = "mm",
                                                  na.rm = FALSE) {
+
                              # Note this must be carried out down here and not in the compute
                              # stat function. Doing so will result in broken connections for
                              # the label aesthetic
                              data <- data |>
                                mutate(
-                                 colour = marker_colours
+                                 label = label_glyph
                                ) |>
                                select(-c(starts_with("marker_")))
 
-                             GeomText$draw_panel(data, panel_params, coord,
+                             GeomMarquee$draw_panel(data, panel_params, coord,
                                                     size.unit = size.unit, na.rm = na.rm)
                            }
 )
