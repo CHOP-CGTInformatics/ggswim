11875f45877651f76378f4e50a9dde284cc448a7
Richard Hanna <HANNAR1@EMAIL.CHOP.EDU>
2024-09-27 14:41:00 -0400

    Remove grid, print, build, check fxs and tests

--- tests/testthat/test-cases.R
+++ tests/testthat/test-cases.R
@@ -3,45 +3,11 @@ test_that("geom_swim_lane works with inherited data", {
     ggplot2::ggplot(mapping = aes(x = start_time, y = pt_id, colour = disease_assessment)) +
     geom_swim_lane(mapping = aes(xend = end_time))
 
-  expect_setequal(class(p), c("ggswim_obj", "gg", "ggplot"))
   expect_true(is_empty(p$layers[[1]]$data))
 
   skip_on_ci()
   vdiffr::expect_doppelganger(
     title = "geom_swim_lane works with inherited data",
-    fig = p
-  )
-})
-
-test_that("geom_swim_point works with inherited data", {
-  p <- infusion_events |>
-    ggplot2::ggplot(mapping = aes(x = time_from_initial_infusion, y = pt_id, colour = infusion_type)) +
-    geom_swim_point()
-
-  expect_setequal(class(p), c("ggswim_obj", "gg", "ggplot"))
-  expect_true(is_empty(p$layers[[1]]$data))
-
-  skip_on_ci()
-  vdiffr::expect_doppelganger(
-    title = "geom_swim_point works with inherited data",
-    fig = p
-  )
-})
-
-test_that("geom_swim_label works with inherited data", {
-  p <- end_study_events |>
-    ggplot2::ggplot(mapping = aes(
-      x = time_from_initial_infusion, y = pt_id,
-      label_vals = end_study_label, label_names = end_study_name
-    )) +
-    geom_swim_point()
-
-  expect_setequal(class(p), c("ggswim_obj", "gg", "ggplot"))
-  expect_true(is_empty(p$layers[[1]]$data))
-
-  skip_on_ci()
-  vdiffr::expect_doppelganger(
-    title = "geom_swim_label works with inherited data",
     fig = p
   )
 })
