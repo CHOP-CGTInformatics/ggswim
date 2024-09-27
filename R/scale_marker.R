418c2d41a6b01c65b6e7bb5c2f3825a74016c34f
Richard Hanna <HANNAR1@EMAIL.CHOP.EDU>
2024-09-27 13:59:56 -0400

    Update marker documentation

--- R/scale_marker.R
+++ R/scale_marker.R
@@ -1,7 +1,19 @@
-
+#' @title Position scales for discrete marker data
+#'
+#' @description
+#' [scale_marker_discrete()] is used to set discrete x aesthetics for swimmer plot
+#' markers.
+#'
+#' @param glyphs Marker glyphs passed to the marker layer, taking on the form of
+#' emojis or ASCII shapes. Default glyphs are provided in the absence of defined
+#' ones.
+#' @param colors Marker colors passed to the marker layer for non-emojis.
+#' Default colors are provided in the absence of defined ones.
+#' @inheritParams ggplot2::scale_x_discrete
+#'
+#' @export
 
 scale_marker_discrete <- function(glyphs = NULL, colors = NULL, limits = NULL, ...) {
-
   markers <- data.frame(glyphs = glyphs, colors = colors, labels = limits) |>
     distinct()
 
@@ -15,23 +27,25 @@ scale_marker_discrete <- function(glyphs = NULL, colors = NULL, limits = NULL, .
                  limits = markers$labels, ..., na.translate = FALSE)
 }
 
-
-.default_glyphs <- c("⛔", "✔", "✖", "☀", "☢", "☠", "☮", "⚛", "♥",
-                     "✀")
-
+#' @noRd
+#' @keywords internal
 pal_markers <- function(glyphs = NULL, colors = NULL, n_values = NULL) {
 
+  # Define colour and glyph lengths via markers supplied or default values
   n_values <- n_values %||% max(length(glyphs), length(colors))
   if (n_values == 0) n_values <- length(.default_glyphs)
 
+  # Create a vctrs list to store colour and glyph values
   markers <- vctrs::new_rcrd(
     list(
-      colour = rep(colors %||% "black", length.out = n_values),
+      colour = rep(colors %||% .default_colours, length.out = n_values),
       glyphs = rep(glyphs %||% .default_glyphs, length.out = n_values)
     ),
     class = "marker"
   )
 
+  # If the values supplied are of a length greater than the default palette,
+  # throw a warning.
   function(n) {
     if (n > n_values) {
       cli::cli_warn(
@@ -42,3 +56,18 @@ pal_markers <- function(glyphs = NULL, colors = NULL, n_values = NULL) {
     markers[seq_len(n)]
   }
 }
+
+#' @title ggswim marker defaults
+#'
+#' @examples
+#' ggswim::.default_glyphs
+#' ggswim::.default_colours
+#'
+#'
+#' @export
+# Set default glyphs and colours
+.default_glyphs <- c("●", "■", "▲", "⬥" ,"▼", "▢", "□", "△", "◇")
+
+#' @rdname dot-default_glyphs
+#' @export
+.default_colours <- scales::brewer_pal(palette = "Set1")(9) # 9 is max for brewer
